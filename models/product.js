19621ee10f3201d5d94c793981ef17bf9f2a39b1
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-03 19:06:05 +0100

    added storing data in a file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +7,38 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+
+    // Read file -> for bigger files use read Stream
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      console.log("read err", err);
+      console.log("read fileContent", fileContent);
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log("write err", err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    console.log("in fetch");
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) return [];
+      return JSON.parse(fileContent);
+    });
   }
 };
