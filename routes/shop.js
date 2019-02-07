0eb55cbdc88dd965a73ffab8dfb3098f44d055d4
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-07 22:03:53 +0100

    extracting dynamic params

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,11 @@ router.get("/", shopController.getIndex);
 // GET
 router.get("/products", shopController.getProducts);
 
+// with dynamic routs order again maters, because eg /products/delete looks like delete is :productId
+// /products/0.2342342
+// GET
+router.get("/products/:productId", shopController.getProduct);
+
 // /cart
 // GET
 router.get("/cart", shopController.getCart);
