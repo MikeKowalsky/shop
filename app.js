1a47be490d5a7b4594e4ac2376c26c688d99b7c6
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-11 00:06:59 +0100

    created user model, added relations and middleware to handle user

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,16 +20,43 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      // Adding new field to every req
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Setting relations
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 // Goes through all models and relations and sync them with db
 // if there is no table for particular model, then it will be created
 sequelize
   .sync()
+  // .sync({ force: true })
   .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Mike", email: "test@test.com" });
+    }
+    // we dont need to put this user in Promise,
+    // it will be done automatically
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     console.log("**** **** **** ****");
     console.log("**** MySQL Connected");
     app.listen(3000, () => {
