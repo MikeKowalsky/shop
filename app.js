5fab9b82d77f2c4d0a41a78c026a90b63c8d0286
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-17 22:09:32 +0100

    conneted MongoDB

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 const errorController = require("./controllers/error");
-
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database");
 
 const app = express();
 
@@ -25,58 +18,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      // Adding new field to every req
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findByPk(1)
+  //   .then(user => {
+  //     // Adding new field to every req
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Setting relations
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-// Goes through all models and relations and sync them with db
-// if there is no table for particular model, then it will be created
-sequelize
-  .sync()
-  // .sync({ force: true })
-  .then(result => {
-    return User.findByPk(1);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: "Mike", email: "test@test.com" });
-    }
-    // we dont need to put this user in Promise,
-    // it will be done automatically
-    return user;
-  })
-  .then(user => {
-    return user.createCart();
-  })
-  .then(cart => {
-    // console.log(user);
-    console.log("**** **** **** ****");
-    console.log("**** MySQL Connected");
-    app.listen(3000, () => {
-      console.log("**** Server runs on port 3000");
-    });
-  })
-  .catch(err => console.log(err));
+mongoConnect(client => {
+  // console.log(client);
+  app.listen(3000, () => {
+    console.log("**** Server runs on port 3000");
+  });
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,20 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "NodeComplete", {
-  dialect: "mysql",
-  host: "localhost"
-});
+const keys = require("./keys");
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    `mongodb+srv://${keys.keys.MONGO_USER}:${
+      keys.keys.MONGO_PASSWORD
+    }@cluster0-idsge.mongodb.net/test?retryWrites=true`,
+    { useNewUrlParser: true }
+  )
+    .then(client => {
+      console.log("**** MongoDB connected");
+      callback(client);
+    })
+    .catch(err => console.log(err));
+};
+
+module.exports = mongoConnect;
