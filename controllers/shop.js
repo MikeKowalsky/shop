4ebc9b248aa2f42105f6ecd761391cf934db3204
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-01 07:05:21 +0100

    added all cart functionalities with mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -69,14 +70,29 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
-    .then(result => res.redirect("/orders"))
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => ({
+        quantity: i.quantity,
+        product: { ...i.productId._doc } //to retrieve whole data not only id, which will be default behavior
+      }));
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products
+      });
+      order.save();
+    })
+    .then(result => req.user.clearCart())
+    .then(() => res.redirect("/orders"))
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then(orders =>
       res.render("shop/orders", {
         path: "/orders",
--- routes/shop.js
+++ routes/shop.js
@@ -32,12 +32,12 @@ router.post("/cart", shopController.postCart);
 // Delete item from cart
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
-// // /create-order
-// // POST
-// router.post("/create-order", shopController.postOrder);
+// /create-order
+// POST
+router.post("/create-order", shopController.postOrder);
 
-// // /orders
-// // GET
-// router.get("/orders", shopController.getOrders);
+// /orders
+// GET
+router.get("/orders", shopController.getOrders);
 
 module.exports = router;
