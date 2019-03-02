2b580962dd19264d4e1cae0beaa7cdf71c3c6be7
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-02 12:16:42 +0100

    implemented simple login view, route and controller

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require("./util/keys");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 const User = require("./models/user");
 
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
