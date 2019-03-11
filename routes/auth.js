eaeaf801b65a7963544220e979632e1c48f2e966
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-11 18:58:35 +0100

    added password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator/check");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
 
@@ -13,14 +13,30 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please enter a valid email.")
-    .custom((value, { req }) => {
-      if (value === "test@test.com")
-        throw new Error("This email is forbidden!");
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        if (value === "test@test.com")
+          throw new Error("This email is forbidden!");
+        return true;
+      }),
+    // another approach - should be the same, but to remeber
+    // check is checking everything - req.body, cookies, header ...
+    body(
+      "password",
+      "Please enter a password with minimum 5 alphanumeric characters!"
+    )
+      // second parameter in body, check is a default msg which will be send to the frontend
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password)
+        throw new Error("Passwords need to match!");
       return true;
-    }),
+    })
+  ],
   //just an exapmle, how we can add the custom validation pattern
   authController.postSignup
 );
