f52a26a68c57f5e4ed39911b91226b3a5a2bc17a
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-07 19:41:30 +0200

    implemented ssl

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const https = require("https");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -38,6 +39,9 @@ const store = new MongoDBStore({
 });
 
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync("server.key");
+const certificate = fs.readFileSync("server.cert");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "images"),
@@ -137,9 +141,11 @@ mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(client => {
     console.log("**** MongoDB connected");
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("**** Server runs on port 3000");
-    });
+    https
+      .createServer({ key: privateKey, cert: certificate }, app)
+      .listen(process.env.PORT || 3000, () => {
+        console.log("**** Server runs on port 3000");
+      });
   })
   .catch(err => {
     console.log(err);
